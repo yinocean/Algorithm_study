525422fe70a8870ff36d688c5eb611d4a025c0a3
Ocean <76829334+yinocean@users.noreply.github.com>
2024-04-24 17:39:17 +0900

    Update JadenCase 문자열 만들기.py
    Add 문제 해결 전 코드

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -6,3 +6,16 @@ def solution(s):
         answer.append(i.capitalize())
         
     return ' '.join(answer)
+    
+# 문제 해결 전 코드
+# def solution(s):
+#     s = s.split(' ')
+#     answer = []
+    
+#     for i in s:
+#         if i[0].islower(): # 첫 글자가 소문자 이기만 하면 title() 함수 사용
+#             answer.append(i.title())
+#         else:
+#             answer.append(i)
+#     return ' '.join(answer)
+# 공백문자 이후 글자가 알파벳이 아닌 경우 핸들링이 안되서 런타임 에러 발생
