8484f3548571de9741bb208b1288763659c5026d
Ocean <76829334+yinocean@users.noreply.github.com>
2024-05-23 17:00:10 +0900

    [level 2] Title: 타겟 넘버, Time: 4.60 ms, Memory: 8.35 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,17 @@
 from collections import deque
-
+# numbers의 첫 인덱스에서부터 모든 케이스를 확인하고 그 다음 인덱스로 넘어간다는 점에서 bfs
 def solution(numbers, target):
+    # 큐 생성
     queue = deque([(0, 0)])  # (current_sum, index)
     count = 0
     
-    while queue:
+    while queue: # 큐가 비지 않을 때까지
         current_sum, index = queue.popleft()
-        
+        # 합이 목표값과 같은지 체크 하고 count 증가
         if index == len(numbers):
             if current_sum == target:
                 count += 1
-        else:
+        else: # numbers 내 숫자를 더하거나 빼는 경우 모두 큐에 추가
             queue.append((current_sum + numbers[index], index + 1))
             queue.append((current_sum - numbers[index], index + 1))
     
